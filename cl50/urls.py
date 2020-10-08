fcbc8015cdbdea6517dc161a0de37ed5c3e27bc5
luiz_bp <carlinhospereira12@hotmail.com>
2020-10-07 23:08:43 -0300

    Primeiro commit

--- cl50/urls.py
+++ cl50/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from cl50.views import calculo_cl50
+from cl50.views import calculo_cl50, teste
 # from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
-    path('<str:comando>/', calculo_cl50),
+    # path('<str:comando>/', calculo_cl50),
+    path('', teste),
     # path(r'^login/', obtain_jwt_token),
 ]
--- cl50/views.py
+++ cl50/views.py
@@ -2,6 +2,7 @@ from cl50.calculos import Calculos
 
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from django.http import JsonResponse
 
@@ -12,3 +13,8 @@ def calculo_cl50(request, comando):
         c = Calculos()
         result = c.cl50(comando=comando)
         return JsonResponse({'CL50':round(float(result[0]),2), 'NEM':round(float(result[1]),2), 'min':round(float(result[2]),2), 'max':round(float(result[3]),2)})
+
+@api_view(['GET','POST'])
+def teste(request):
+    if request.method == 'POST':
+        return Response(request.data)
